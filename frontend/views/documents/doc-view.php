9f7825539a8e6cc1a0ba0bceb810a80df5c5aaf8
iD-House <idadajon@gmail.com>
2023-08-02 18:30:26 +0500

    aasd

--- frontend/views/documents/doc-view.php
+++ frontend/views/documents/doc-view.php
@@ -45,14 +45,14 @@ $domen = Url::base('https');
 
         <?php
 
-        echo \lesha724\documentviewer\GoogleDocumentViewer::widget([
-            'url' => $domen . $model->path,//url на ваш документ
-            'width' => '100%',
-            'height' => '600px',
-            //https://geektimes.ru/post/111647/
-            'embedded' => true,
-            'a' => \lesha724\documentviewer\GoogleDocumentViewer::A_BI //A_V = 'v', A_GT= 'gt', A_BI = 'bi'
-        ]);
+//        echo \lesha724\documentviewer\GoogleDocumentViewer::widget([
+//            'url' => $domen . $model->path,//url на ваш документ
+//            'width' => '100%',
+//            'height' => '600px',
+//            //https://geektimes.ru/post/111647/
+//            'embedded' => true,
+//            'a' => \lesha724\documentviewer\GoogleDocumentViewer::A_BI //A_V = 'v', A_GT= 'gt', A_BI = 'bi'
+//        ]);
         ?>
     </div>
 </div>
