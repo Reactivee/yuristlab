30d7574199a0101226bfb6aff961db26c905fe0a
iD-House <idadajon@gmail.com>
2023-08-02 18:41:33 +0500

    aasdasd

--- frontend/views/documents/doc-view-template.php
+++ frontend/views/documents/doc-view-template.php
@@ -12,15 +12,18 @@ $domen = Url::base('https');
 ?>
 <div class="row">
     <div class="container-fluid px-5">
-        <?php echo \lesha724\documentviewer\GoogleDocumentViewer::widget([
-            'url' => $domen . $model->path,//url на ваш документ
-            'width' => '100%',
-            'height' => '600px',
-            //https://geektimes.ru/post/111647/
-            'embedded' => true,
-            'a' => \lesha724\documentviewer\GoogleDocumentViewer::A_V //A_V = 'v', A_GT= 'gt', A_BI = 'bi'
-        ]);
-        ?>
+        <iframe src='https://view.officeapps.live.com/op/embed.aspx?src=<?= $domen . $model->path?>'
+                width='100%' height='900px' frameborder='0'>
+        </iframe>
+<!--        --><?php //echo \lesha724\documentviewer\GoogleDocumentViewer::widget([
+//            'url' => $domen . $model->path,//url на ваш документ
+//            'width' => '100%',
+//            'height' => '600px',
+//            //https://geektimes.ru/post/111647/
+//            'embedded' => true,
+//            'a' => \lesha724\documentviewer\GoogleDocumentViewer::A_V //A_V = 'v', A_GT= 'gt', A_BI = 'bi'
+//        ]);
+//        ?>
     </div>
 </div>
 
