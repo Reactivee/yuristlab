47bd11a60b8d24556d5e9013662787b324a2ef71
iD-House <idadajon@gmail.com>
2023-08-16 13:41:33 +0500

    aa

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,6 +21,7 @@ AppAsset::register($this);
         <meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?php $this->registerCsrfMetaTags() ?>
+        <?php echo Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
--- frontend/views/layouts/top.php
+++ frontend/views/layouts/top.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 ?>
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper align-items-center">
-        <a class="navbar-brand brand-logo" href="index.html"><img src="../../images/logo.svg" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="../../images/logo-mini.svg" alt="logo"/></a>
+        <a class="navbar-brand brand-logo" href="/"><img src="../../images/logo.png" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="/"><img src="../../images/logo.png" alt="logo"/></a>
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-sort-variant"></span>
         </button>
