821c71ed8b1e378b8edb13ead20fda9ecb71b0e5
iD-House <idadajon@gmail.com>
2023-09-19 18:20:27 +0500

    ыыыasd

--- frontend/views/lawyer/team_profile.php
+++ frontend/views/lawyer/team_profile.php
@@ -286,7 +286,7 @@ use yii\bootstrap4\Html;
                     <?= $form->field($user_form, 'new_pass')->passwordInput(['maxlength' => true])->label('Yangi parol') ?>
                     <?= $form->field($user_form, 'new_pass_confirm')->passwordInput(['maxlength' => true])->label('Parolni tasdiqlash') ?>
 
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-success mt-2']) ?>
+                    <?= Html::submitButton('Saqlash', ['class' => 'btn btn-success mt-2']) ?>
 
                     <!--                        <button type="submit" class="btn btn-primary mr-2">Saqlash</button>-->
                     <?php ActiveForm::end(); ?>
@@ -295,151 +295,90 @@ use yii\bootstrap4\Html;
             </div>
             <div class="tab-pane fade" id="editor">
                 <div class="card-body">
-                    <h4 class="card-title">Horizontal Two column</h4>
                     <form class="form-sample">
-                        <p class="card-description">
-                            Personal info
-                        </p>
+
                         <div class="row">
-                            <div class="col-md-6">
+                            <h4 class="card-title">Xodim ish joylari</h4>
+
+                            <div class="col-md-12 work-form">
                                 <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">First Name</label>
-                                    <div class="col-sm-9">
+                                    <label class="col-sm-2 col-form-label card-title">Ish joyi</label>
+                                    <div class="col-sm-10">
                                         <input type="text" class="form-control">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
                                 <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Last Name</label>
-                                    <div class="col-sm-9">
+                                    <label class="col-sm-2 col-form-label card-title">Lavozimi</label>
+                                    <div class="col-sm-10">
                                         <input type="text" class="form-control">
                                     </div>
                                 </div>
+
+                                <div class="form-group ">
+                                    <h4 class="card-title">Ishlagan davr</h4>
+                                    <div class="input-group input-daterange d-flex align-items-center">
+                                        <input type="text" class="form-control" value="2022-04-05">
+                                        <div class="input-group-addon mx-4">gacha</div>
+                                        <input type="text" class="form-control" value="2023-04-19">
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-warning text-right">+</button>
+                                <hr>
+
                             </div>
+
                         </div>
                         <div class="row">
-                            <div class="col-md-6">
+                            <h4 class="card-title">Ta'lim davri</h4>
+
+                            <div class="col-md-12 work-form">
                                 <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Gender</label>
-                                    <div class="col-sm-9">
-                                        <select class="form-control">
-                                            <option>Male</option>
-                                            <option>Female</option>
-                                        </select>
+                                    <label class="col-sm-2 col-form-label card-title">O'quv muassasasi</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
                                 <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Date of Birth</label>
-                                    <div class="col-sm-9">
-                                        <input class="form-control" placeholder="dd/mm/yyyy">
+                                    <label class="col-sm-2 col-form-label card-title">Lavozimi</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>
+
+                                <div class="form-group ">
+                                    <h4 class="card-title">O'quv davr</h4>
+                                    <div class="input-group input-daterange d-flex align-items-center">
+                                        <input type="text" class="form-control" value="2022-04-05">
+                                        <div class="input-group-addon mx-4">gacha</div>
+                                        <input type="text" class="form-control" value="2023-04-19">
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-warning text-right">+</button>
+                                <hr>
+
                             </div>
+
                         </div>
                         <div class="row">
-                            <div class="col-md-6">
+                            <h4 class="card-title">Xobbilar</h4>
+
+                            <div class="col-md-12 work-form">
                                 <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Category</label>
-                                    <div class="col-sm-9">
-                                        <select class="form-control">
-                                            <option>Category1</option>
-                                            <option>Category2</option>
-                                            <option>Category3</option>
-                                            <option>Category4</option>
-                                        </select>
+                                    <label class="col-sm-2 col-form-label card-title">O'quv muassasasi</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>
+
+                                <button type="button" class="btn btn-warning text-right">+</button>
+                                <hr>
+
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Membership</label>
-                                    <div class="col-sm-4">
-                                        <div class="form-check">
-                                            <label class="form-check-label">
-                                                <input type="radio" class="form-check-input" name="membershipRadios"
-                                                       id="membershipRadios1" value="" checked="">
-                                                Free
-                                                <i class="input-helper"></i></label>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-5">
-                                        <div class="form-check">
-                                            <label class="form-check-label">
-                                                <input type="radio" class="form-check-input" name="membershipRadios"
-                                                       id="membershipRadios2" value="option2">
-                                                Professional
-                                                <i class="input-helper"></i></label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
                         </div>
-                        <p class="card-description">
-                            Address
-                        </p>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Address 1</label>
-                                    <div class="col-sm-9">
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">State</label>
-                                    <div class="col-sm-9">
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Address 2</label>
-                                    <div class="col-sm-9">
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Postcode</label>
-                                    <div class="col-sm-9">
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">City</label>
-                                    <div class="col-sm-9">
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Country</label>
-                                    <div class="col-sm-9">
-                                        <select class="form-control">
-                                            <option>America</option>
-                                            <option>Italy</option>
-                                            <option>Russia</option>
-                                            <option>Britain</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+
+                        <?= Html::button('Saqlash', ['class' => 'btn btn-success mt-2']) ?>
+
                 </div>
             </div>
 
