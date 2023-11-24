348eb555ea5712de24011b55ba99e9c67d62c3a5
iD-House <idadajon@gmail.com>
2023-11-24 03:07:55 +0200

    aa

--- frontend/views/news/content.php
+++ frontend/views/news/content.php
@@ -10,6 +10,21 @@
             </div>
             <h2 class="text-black news_poster_title my-4">
                 <?= $content->title_uz ?>
+                <div class="short_info d-flex align-items-center mt-3">
+                    <div class="author mr-3">
+                        <i class="mdi mdi-account-outline"></i>
+                        <span>Name</span>
+                    </div>
+                    <div class="news_views mr-3">
+                        <i class="mdi mdi-eye"></i>
+                        <span>1000</span>
+                    </div>
+                    <div class="news_category mr-3">
+                        <i class="mdi mdi-file-document-box"></i>
+                        <span>Blog</span>
+                    </div>
+                    <div class="shareit"    ></div>
+                </div>
             </h2>
         </div>
         <div class="sub_title_news">
